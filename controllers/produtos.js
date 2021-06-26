d5228bf8176b29e76a5625ab74ac1afc5893674a
FredBoaretti <frederico.boaretti@gmail.com>
2021-06-26 17:43:03 -0300

    Nova Atualizacao

--- controllers/produtos.js
+++ controllers/produtos.js
@@ -3,36 +3,55 @@ const Produto = require("../models/produtos")
 
 const route = express.Router()
 
+route.get("/produto", async (req, res) => {
+    const { id } = req.query
+
+    const produto = await Produto.findOne({ _id: id })
+
+    if (produto) {
+        res.send(produto)
+    } else {
+        res.send("Produto não encontrado")
+    }
+})
+
 route.post("/produtos", async (req, res) => {
     await Produto.create(req.body, (err, ret) => {
         if (err)
             return res.send(err.message)
-        return res.send(ret)
+        return res.send("Produto cadastrado com sucesso.")
     })
 })
 
-
 route.put("/produtos", async (req, res) => {
-    const { _id,  preco,nome,imagem,descricao, quantidade,  ativo } = req.body
-    var dados = await Produto.findOne({ _id: _id })
-    if (dados == null) {
+    const { _id, preco, nome, imagem, descricao, quantidade } = req.body
+
+    const produto = await Produto.findOne({ _id: _id })
+
+    if (produto) {
+        await Produto.updateOne(
+          { _id: _id },
+          { $set: { nome: nome, preco: preco, imagem: imagem, descricao: descricao, quantidade: quantidade }}
+        )
+        res.send("Produto atualizado com sucesso!")
+    } else {
         res.send("Produto não encontrado")
-        return false
     }
-        var mudanca = 'Produto foi alterado com sucesso! \n';
-    await Produto.updateOne({ _id: _id }, { $set: { nome: nome, preco: preco, imagem: imagem, descricao: descricao, quantidade: quantidade }})
-    res.send(mudanca)
 })
 
 route.delete("/produtos", async (req, res) => {
-    const { _id } = req.body
-    var dados = await Produto.findOne({ _id: _id })
+    const { id } = req.query
+
+    const dados = await Produto.findOne({ _id: id })
+
     if (dados == null) {
         res.send("Produto não encontrado")
         return false
     }
-    var mudanca = "O produto "+dados.nome+" foi apagado!";
-    await Produto.updateOne({ _id: _id }, { $set: { ativo: false }})
+    const mudanca = "O produto "+dados.nome+" foi apagado!";
+
+    await Produto.updateOne({ _id: id }, { $set: { ativo: false }})
+
     res.send(mudanca);
 })
 
